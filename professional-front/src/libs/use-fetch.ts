0863ae5ab06a407acb7f960a8638eadb4f3e48f2
z1the3 <1813783831@qq.com>
2024-04-04 01:22:16 +0800

    feat: 增加useFetch

---
+++ professional-front/src/libs/use-fetch.ts
@@ -0,0 +1,42 @@
+import useSWR from "swr";
+
+
+type provideMethod = 'GET' | 'POST' | 'PATCH'| 'DELETE'
+const getToken = () => {
+    return window.localStorage.getItem("pt-auth")||""
+}
+// 适用于get请求与body参数为json类型的请求
+const dataFetcher = async (url: string, method: provideMethod, params: any) => {
+    const newParams = {
+        Authorization: getToken(),
+        params
+    }
+    const res = await fetch(url, {
+        method,
+        headers: {
+            ...
+            method === "GET" ? {} : {
+                "Content-Type": "application/json",
+                body: JSON.stringify(newParams)
+            }
+        }
+    }).then(async(r) => {
+        const res = await r.json()
+        // 这里把非预期错误状态码全拦住，保证useFetch里是成功情况+用户预期错误情况
+        if (res.code === 404) {
+            return Promise.reject(new Error(`${res.code}: ${res.message}`))
+        }
+        return res
+    }).catch(e => {
+        throw e
+    })
+    return res
+}
+
+
+
+export default function useFetch(params: {url: string, method: provideMethod, params: any}) {
+    const {url, method, params:fetchParams} = params
+    const {data,error,isLoading,mutate } = useSWR([url,method,fetchParams], ([url,method,fetchParams])=>dataFetcher(url,method,fetchParams))
+    return {data,error,isLoading,mutate}
+}
--- professional-front/src/pages/index.tsx
+++ professional-front/src/pages/index.tsx
@@ -2,10 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
+import useFetch from "@/libs/use-fetch";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // ex.发请求
+
+  const { data } = useFetch({
+    url: 'http://127.0.0.1:4523/m1/4275099-0-default/pet/4',
+    method: 'POST',
+    params: { b: '1' }
+  })
   return (
     <>
       <Head>
