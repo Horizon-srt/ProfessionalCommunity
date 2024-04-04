ccba240b7c35ebc06d7035ae5caedf2cb6cfdb31
Horizon-srt <horizon_srt@outlook.com>
2024-04-04 12:54:56 +0800

    配置eslint和antdesign

--- professional-front/src/libs/use-fetch.ts
+++ professional-front/src/libs/use-fetch.ts
@@ -1,42 +1,47 @@
-import useSWR from "swr";
+import useSWR from 'swr';
+import { ProvideMethod } from '@/utils/data-types';
 
-
-type provideMethod = 'GET' | 'POST' | 'PATCH'| 'DELETE'
 const getToken = () => {
-    return window.localStorage.getItem("pt-auth")||""
-}
+  return window.localStorage.getItem('pt-auth') || '';
+};
+
 // 适用于get请求与body参数为json类型的请求
-const dataFetcher = async (url: string, method: provideMethod, params: any) => {
-    const newParams = {
-        Authorization: getToken(),
-        params
+const dataFetcher = async (url: string, method: ProvideMethod, params: any) => {
+  const newParams = {
+    Authorization: getToken(),
+    params
+  };
+  const res = await fetch(url, {
+    method,
+    headers: {
+      ...
+      method === 'GET' ? {} : {
+        'Content-Type': 'application/json',
+        body: JSON.stringify(newParams)
+      }
     }
-    const res = await fetch(url, {
-        method,
-        headers: {
-            ...
-            method === "GET" ? {} : {
-                "Content-Type": "application/json",
-                body: JSON.stringify(newParams)
-            }
-        }
-    }).then(async(r) => {
-        const res = await r.json()
-        // 这里把非预期错误状态码全拦住，保证useFetch里是成功情况+用户预期错误情况
-        if (res.code === 404) {
-            return Promise.reject(new Error(`${res.code}: ${res.message}`))
-        }
-        return res
-    }).catch(e => {
-        throw e
-    })
-    return res
-}
+  }).then(async(r) => {
+    const res = await r.json();
+    // 这里把非预期错误状态码全拦住，保证useFetch里是成功情况+用户预期错误情况
+    if (res.code === 404) {
+      return Promise.reject(new Error(`${res.code}: ${res.message}`));
+    }
+    return res;
+  }).catch(e => {
+    throw e;
+  });
+  return res;
+};
 
-
-
-export default function useFetch(params: {url: string, method: provideMethod, params: any}) {
-    const {url, method, params:fetchParams} = params
-    const {data,error,isLoading,mutate } = useSWR([url,method,fetchParams], ([url,method,fetchParams])=>dataFetcher(url,method,fetchParams))
-    return {data,error,isLoading,mutate}
+export default function useFetch(params: {
+  url: string,
+  method: ProvideMethod,
+  params: any
+}) {
+  const { url, method, params:fetchParams } = params;
+  const { data, error, isLoading, mutate } = useSWR(
+    [url, method, fetchParams],
+    ([url, method, fetchParams]) => dataFetcher(url, method, fetchParams)
+  );
+  return { data, error, isLoading, mutate };
 }
---
+++ professional-front/src/pages/tourist/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tourist: React.FC = () => {
+  return (
+    <main>
+      <h1>Tourist page</h1>
+    </main>
+  );
+};
+
+export default Tourist;
