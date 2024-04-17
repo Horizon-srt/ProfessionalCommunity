290dfbacbb795dde8cd1b430741a4f9bde5b7952
Horizon-srt <horizon_srt@outlook.com>
2024-04-17 19:57:53 +0800

    社区服务模块

--- professional-front/src/app/normal/service/page.tsx
+++ professional-front/src/app/normal/service/page.tsx
@@ -1,12 +1,9 @@
 'use client';
+import ServiceParts from '@/components/ServiceParts';
 import React from 'react';
 
 const Family: React.FC = () => {
-  return (
-    <div>
-      <h1>Tourist service page</h1>
-    </div>
-  );
+  return <ServiceParts isAdmin={true} />;
 };
 
 export default Family;
