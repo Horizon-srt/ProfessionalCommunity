abbdca693bc21c09aa7f092314be0a9f4421c37a
Horizon-srt <horizon_srt@outlook.com>
2024-05-03 01:26:37 +0800

    编辑服务逻辑部分

---
+++ professional-front/src/app/admin/service/edit/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import ServiceParts from '@/components/ServiceParts';
+import React from 'react';
+
+const Family: React.FC = () => {
+  return <ServiceParts isAdmin={true} />;
+};
+
+export default Family;
---
+++ professional-front/src/components/ServiceEdit/index.tsx
@@ -0,0 +1,527 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Col,
+  Form,
+  Image,
+  Input,
+  Radio,
+  Row,
+  Spin,
+  Upload,
+  message
+} from 'antd';
+import Link from 'next/link';
+import style from '@/components/ServiceEdit/styles/style.module.css';
+import useFetch, { useFetchMutation } from '@/services/use-fetch';
+import Card from '@/components/Card';
+import { PlusOutlined } from '@ant-design/icons';
+import { ProvideMethod } from '@/types/data-types';
+import { useRouter } from 'next/navigation';
+
+interface ServiceEditProps {
+  title: string;
+  sid: string;
+}
+
+const ServiceEdit: React.FC<ServiceEditProps> = ({ title, sid }) => {
+  const [cover, setCover] = useState<string | ArrayBuffer>('');
+  const [map, setMap] = useState<string | ArrayBuffer>('');
+  const [video, setVideo] = useState<string | ArrayBuffer>('');
+
+  const router = useRouter();
+
+  const defaultCreateParams = {
+    url: '/services',
+    method: 'POST' as ProvideMethod,
+    params: null
+  };
+
+  const defaultChangeParams = {
+    url: '/services/' + sid,
+    method: 'PATCH' as ProvideMethod,
+    params: null
+  };
+
+  const defaultDeleteParams = {
+    url: '/services/' + sid,
+    method: 'PATCH' as ProvideMethod,
+    params: null
+  };
+
+  const {
+    data: createReturnData,
+    error: createReturnError,
+    trigger: createService
+  } = useFetchMutation(defaultCreateParams);
+
+  const {
+    data: changeReturnData,
+    error: changeReturnError,
+    trigger: changeService
+  } = useFetchMutation(defaultChangeParams);
+
+  const {
+    data: deleteReturnData,
+    error: deleteReturnError,
+    trigger: deleteService
+  } = useFetchMutation(defaultDeleteParams);
+
+  const beforeUploadImg = (file: any) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    return isJpgOrPng;
+  };
+
+  const beforeUploadVideo = (file: any) => {
+    const isMp4 = file.type === 'video/mp4';
+    if (!isMp4) {
+      message.error('You can only upload MP4 file!');
+    }
+    const isLt50M = file.size / 1024 / 1024 < 50;
+    if (!isLt50M) {
+      message.error('Image must smaller than 50MB!');
+    }
+    return isMp4;
+  };
+
+  // const handleCover = async (info: any) => {
+  //   if (info.file.status === 'done') {
+  //     const reader = new FileReader();
+  //     reader.addEventListener('load', () => setCover(reader.result || ''));
+  //     reader.readAsDataURL(info.file.originFileObj);
+  //   }
+  // };
+
+  // const handleMap = async (info: any) => {
+  //   if (info.file.status === 'done') {
+  //     const reader = new FileReader();
+  //     reader.addEventListener('load', () => setMap(reader.result || ''));
+  //     reader.readAsDataURL(info.file.originFileObj);
+  //   }
+  // };
+
+  // const handleVideo = async (info: any) => {
+  //   if (info.file.status === 'done') {
+  //     const reader = new FileReader();
+  //     reader.addEventListener('load', () => setVideo(reader.result || ''));
+  //     reader.readAsDataURL(info.file.originFileObj);
+  //   }
+  // };
+
+  const normFile = (e: any) => {
+    // TODO: 视频转换成base64
+    if (Array.isArray(e)) {
+      return e[0];
+    }
+    return e?.fileList[0];
+  };
+
+  const onCreateFinish = async (values: any) => {
+    const finalValue = {
+      ...values,
+      cover: values.cover?.thumbUrl ? values.cover.thumbUrl : '',
+      map: values.map?.thumbUrl ? values.map.thumbUrl : '',
+      video: values.video?.thumbUrl ? values.video.thumbUrl : ''
+    };
+
+    console.log(finalValue);
+
+    // if (sid !== '-1') {
+    //   changeService({ ...defaultChangeParams, params: finalValue });
+    // } else {
+    //   createService({ ...defaultCreateParams, params: finalValue });
+    // }
+  };
+
+  const onFinishFailed = async (e: any) => {
+    message.error(e);
+  };
+
+  const CreateForm: React.FC = () => {
+    const [serviceType, setServiceType] = useState('FIXED');
+
+    return (
+      <Form
+        onFinish={onCreateFinish}
+        onFinishFailed={onFinishFailed}
+        layout='vertical'
+      >
+        <Form.Item
+          name={'name'}
+          label={'Name'}
+          rules={[{ required: true, message: 'Please input service name!' }]}
+        >
+          <Input className={style.inputStyle} placeholder={'Service Name'} />
+        </Form.Item>
+        <Form.Item
+          name={'available'}
+          label={'Available Time'}
+          rules={[{ required: true, message: 'Please input available time!' }]}
+        >
+          <Input className={style.inputStyle} placeholder={'Available Time'} />
+        </Form.Item>
+        <Form.Item
+          name={'detail'}
+          label={'Detail'}
+          rules={[{ required: true, message: 'Please input detail!' }]}
+        >
+          <Input.TextArea
+            className={style.inputStyle}
+            placeholder={'Available Time'}
+          />
+        </Form.Item>
+        <Form.Item
+          label={'Cover'}
+          // valuePropName='fileList'
+          name={'cover'}
+          getValueFromEvent={normFile}
+        >
+          <Upload
+            // action='/upload.do'
+            listType='picture-card'
+            multiple={false}
+            maxCount={1}
+            beforeUpload={beforeUploadImg}
+            // onChange={handleCover}
+          >
+            <button style={{ border: 0, background: 'none' }} type='button'>
+              <PlusOutlined />
+              <div style={{ marginTop: 8 }}>Upload</div>
+            </button>
+          </Upload>
+        </Form.Item>
+        <Form.Item label={'Service Type'}>
+          <Radio.Group
+            name='type'
+            defaultValue={'FIXED'}
+            onChange={e => {
+              setServiceType(e.target.value);
+            }}
+          >
+            <Radio value={'FIXED'}>Fixed</Radio>
+            <Radio value={'ONDOOR'}>Ondoor</Radio>
+          </Radio.Group>
+        </Form.Item>
+        {serviceType === 'FIXED' ? (
+          <>
+            <Form.Item
+              name={'location'}
+              label={'Location'}
+              rules={[
+                { required: true, message: 'Please input service location!' }
+              ]}
+            >
+              <Input
+                className={style.inputStyle}
+                placeholder={'Service Location'}
+              />
+            </Form.Item>
+            <Form.Item
+              label={'Map'}
+              // valuePropName='fileList'
+              name={'map'}
+              getValueFromEvent={normFile}
+            >
+              <Upload
+                // action='/upload.do'
+                listType='picture-card'
+                multiple={false}
+                maxCount={1}
+                beforeUpload={beforeUploadImg}
+                // onChange={handleMap}
+              >
+                <button style={{ border: 0, background: 'none' }} type='button'>
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </button>
+              </Upload>
+            </Form.Item>
+            <Form.Item
+              label={'Video'}
+              // valuePropName='fileList'
+              name={'video'}
+              getValueFromEvent={normFile}
+            >
+              <Upload
+                // action='/upload.do'
+                listType='picture-card'
+                multiple={false}
+                maxCount={1}
+                beforeUpload={beforeUploadVideo}
+                // onChange={handleVideo}
+              >
+                <button style={{ border: 0, background: 'none' }} type='button'>
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </button>
+              </Upload>
+            </Form.Item>
+          </>
+        ) : (
+          <>
+            <Form.Item
+              name={'line'}
+              label={'Line'}
+              rules={[
+                { required: true, message: 'Please input service location!' }
+              ]}
+            >
+              <Input
+                className={style.inputStyle}
+                placeholder={'Service Location'}
+              />
+            </Form.Item>
+          </>
+        )}
+        <div className={style.buttonArea}>
+          <Form.Item>
+            <ReturnButton />
+          </Form.Item>
+          <Form.Item>
+            <DeleteButton />
+          </Form.Item>
+          <Form.Item>
+            <SubmitButton />
+          </Form.Item>
+        </div>
+      </Form>
+    );
+  };
+
+  const EditForm: React.FC = () => {
+    // const { data, error, isLoading } = useFetch({
+    //   url: `/services/${sid}`,
+    //   method: 'GET',
+    //   params: {}
+    // });
+
+    const data = {
+      sid: 'aa',
+      name: 'string',
+      available: 'string',
+      detail: 'string',
+      type: 'ONDOOR',
+      // 针对不同服务类型
+      location: 'string',
+      map: 'string',
+      video: 'dada',
+      line: 'dadadad'
+    };
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [form] = Form.useForm();
+
+    useEffect(() => {
+      if (!isLoading) {
+        form.setFieldsValue(data);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isLoading]);
+
+    useEffect(() => {
+      setIsLoading(false);
+    }, []);
+
+    return (
+      <>
+        {isLoading ? (
+          <></>
+        ) : (
+          <Form
+            onFinish={onCreateFinish}
+            onFinishFailed={onFinishFailed}
+            layout='vertical'
+            form={form}
+          >
+            <Form.Item
+              name={'name'}
+              label={'Name'}
+              rules={[
+                { required: true, message: 'Please input service name!' }
+              ]}
+            >
+              <Input
+                className={style.inputStyle}
+                placeholder={'Service Name'}
+              />
+            </Form.Item>
+            <Form.Item
+              name={'available'}
+              label={'Available Time'}
+              rules={[
+                { required: true, message: 'Please input available time!' }
+              ]}
+            >
+              <Input
+                className={style.inputStyle}
+                placeholder={'Available Time'}
+              />
+            </Form.Item>
+            <Form.Item
+              name={'detail'}
+              label={'Detail'}
+              rules={[{ required: true, message: 'Please input detail!' }]}
+            >
+              <Input.TextArea
+                className={style.inputStyle}
+                placeholder={'Available Time'}
+              />
+            </Form.Item>
+            {/* <Form.Item
+              label={'Cover'}
+              // valuePropName='fileList'
+              name={'cover'}
+              getValueFromEvent={normFile}
+            >
+              <Upload
+                action='/upload.do'
+                listType='picture-card'
+                multiple={false}
+                maxCount={1}
+              >
+                <button style={{ border: 0, background: 'none' }} type='button'>
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </button>
+              </Upload>
+            </Form.Item> */}
+            <Form.Item label={'Service Type'}>
+              <Radio.Group name='type' defaultValue={data.type} disabled>
+                <Radio value={'FIXED'}>Fixed</Radio>
+                <Radio value={'ONDOOR'}>Ondoor</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {data.type === 'FIXED' ? (
+              <>
+                <Form.Item
+                  name={'location'}
+                  label={'Location'}
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input service location!'
+                    }
+                  ]}
+                >
+                  <Input
+                    className={style.inputStyle}
+                    placeholder={'Service Location'}
+                  />
+                </Form.Item>
+                {/* <Form.Item
+                  label={'map'}
+                  // valuePropName='fileList'
+                  name={'Map'}
+                  getValueFromEvent={normFile}
+                >
+                  <Upload
+                    action='/upload.do'
+                    listType='picture-card'
+                    multiple={false}
+                    maxCount={1}
+                  >
+                    <button
+                      style={{ border: 0, background: 'none' }}
+                      type='button'
+                    >
+                      <PlusOutlined />
+                      <div style={{ marginTop: 8 }}>Upload</div>
+                    </button>
+                  </Upload>
+                </Form.Item>
+                <Form.Item
+                  label={'video'}
+                  // valuePropName='fileList'
+                  name={'Video'}
+                  getValueFromEvent={normFile}
+                >
+                  <Upload
+                    action='/upload.do'
+                    listType='picture-card'
+                    multiple={false}
+                    maxCount={1}
+                  >
+                    <button
+                      style={{ border: 0, background: 'none' }}
+                      type='button'
+                    >
+                      <PlusOutlined />
+                      <div style={{ marginTop: 8 }}>Upload</div>
+                    </button>
+                  </Upload>
+                </Form.Item> */}
+              </>
+            ) : (
+              <>
+                <Form.Item
+                  name={'line'}
+                  label={'Line'}
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input service location!'
+                    }
+                  ]}
+                >
+                  <Input
+                    className={style.inputStyle}
+                    placeholder={'Service Line'}
+                  />
+                </Form.Item>
+              </>
+            )}
+            <div className={style.buttonArea}>
+              <Form.Item>
+                <ReturnButton />
+              </Form.Item>
+              <Form.Item>
+                <DeleteButton />
+              </Form.Item>
+              <Form.Item>
+                <SubmitButton />
+              </Form.Item>
+            </div>
+          </Form>
+        )}
+      </>
+    );
+  };
+
+  const ReturnButton: React.FC = () => {
+    return <Button>Return</Button>;
+  };
+
+  const DeleteButton: React.FC = () => {
+    return (
+      <Button
+        type='primary'
+        danger
+        onClick={() => {
+          deleteService({ ...defaultDeleteParams, params: {} });
+          router.push('/admin/service');
+        }}
+      >
+        Delete
+      </Button>
+    );
+  };
+
+  const SubmitButton: React.FC = () => {
+    return (
+      <Button htmlType='submit' type='primary'>
+        Submit
+      </Button>
+    );
+  };
+
+  return (
+    <Card title={title}>{sid === '-1' ? <CreateForm /> : <EditForm />}</Card>
+  );
+};
+
+export default ServiceEdit;
